d3dc1defab883e5cc763414486e599cb77802c6c
Water-Drop <water_drop@foxmail.com>
2014-06-07 21:32:10 +0800

    finish BrokerDaoImpl

--- Code/Trader/src/dao/BrokerDaoImpl.java
+++ Code/Trader/src/dao/BrokerDaoImpl.java
@@ -11,13 +11,52 @@ import java.util.ArrayList;
 import util.JDBCUtil;
 import entity.BrokerInfo;
 
-public class BrokerDaoImpl implements BrokerDao{	
-		public List<BrokerInfo> getAllBrokers(){
+public class BrokerDaoImpl implements BrokerDao {
+	private JDBCUtil jdbc = new JDBCUtil("dcetsT1");
+
+	public List<BrokerInfo> getAllBrokers(){
 			List<BrokerInfo> brokers = new ArrayList<BrokerInfo>();
+			Connection conn = jdbc.getConnection();
+			String sql = "select * from brokerinfo;";
+			Statement st = null;
+			ResultSet rs = null;
+			try {
+				st = conn.createStatement();
+				rs= st.executeQuery(sql);
+				while (rs.next())
+				{
+					BrokerInfo broker = new BrokerInfo();
+					broker.setId(rs.getInt("id"));
+					broker.setName(rs.getString("name"));
+					broker.setIp(rs.getString("ip"));
+					broker.setPort(rs.getInt("port"));
+					brokers.add(broker);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				jdbc.close(conn, st, rs);
+			}
 			return brokers;
 		}
-		public int addBroker(String name, String ip, int port){
-			int id = 0;
-			return id;
+
+	public int addBroker(String name, String ip, int port) {
+		int rtn = 0;
+		Connection conn = jdbc.getConnection();
+		String sql = "insert into brokerinfo (name, ip, port) values (?, ?, ?);";
+		PreparedStatement pst = null;
+		try {
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, name);
+			pst.setString(2, ip);
+			pst.setInt(3,  port);
+			pst.execute();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			jdbc.close(conn, pst, null);
 		}
+		return rtn;
+	}
 }
