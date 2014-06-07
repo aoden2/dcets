3a38f0fe4522e8eab45a3373475e8e2aedcd2644
Water-Drop <water_drop@foxmail.com>
2014-06-07 23:11:29 +0800

    sync

--- Code/Trader/src/entity/OriginOrder.java
+++ Code/Trader/src/entity/OriginOrder.java
@@ -4,43 +4,56 @@ public class OriginOrder {
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getFid() {
 		return fid;
 	}
+
 	public void setFid(int fid) {
 		this.fid = fid;
 	}
+
 	public int getBid() {
 		return bid;
 	}
+
 	public void setBid(int bid) {
 		this.bid = bid;
 	}
+
 	public int getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	public int getStatus() {
 		return status;
 	}
+
 	public void setStatus(int status) {
 		this.status = status;
 	}
+
 	public OriginOrder() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public OriginOrder(int id, int fid, int bid, int quantity, int price,
 			int status) {
 		super();
