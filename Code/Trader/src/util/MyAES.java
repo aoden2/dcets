75d157a4e0ab6910188b952ca84a233274e62779
Water-Drop <water_drop@foxmail.com>
2014-06-07 22:51:14 +0800

    add FIUX related and socket client related
    Add OriginOrderFix Helper

---
+++ Code/Trader/src/util/MyAES.java
@@ -0,0 +1,78 @@
+package util;
+
+/*
+ * Author   : Zhou Cheng
+ * Date     : 2014-6-6
+ * Project  : SocketServer
+ * Filename : MyAES.java
+ * 
+ * All rights reserved.
+ */
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public class MyAES {
+	public static String encrypt(String content, String password) {
+		String result = null;
+		try {
+			KeyGenerator kgen = KeyGenerator.getInstance("AES");
+			kgen.init(128, new SecureRandom(password.getBytes()));
+			SecretKey secretKey = kgen.generateKey();
+			SecretKeySpec key = new SecretKeySpec(secretKey.getEncoded(), "AES");
+			Cipher cipher = Cipher.getInstance("AES");
+			byte[] byteContent = content.getBytes("UTF-8");
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			byte[] resultByte = cipher.doFinal(byteContent);
+			result = parseByte2HexStr(resultByte);  
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public static String decrypt(String content, String password) {
+		String result = null;
+		try {
+			byte[] data = parseHexStr2Byte(content);
+			KeyGenerator kgen = KeyGenerator.getInstance("AES");
+			kgen.init(128, new SecureRandom(password.getBytes()));
+			SecretKey secretKey = kgen.generateKey();
+			SecretKeySpec key = new SecretKeySpec(secretKey.getEncoded(), "AES");
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			byte[] resultByte = cipher.doFinal(data);
+			result = new String(resultByte, "UTF-8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public static String parseByte2HexStr(byte buf[]) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < buf.length; i++) {
+			String hex = Integer.toHexString(buf[i] & 0xFF);
+			if (hex.length() == 1) {
+				hex = '0' + hex;
+			}
+			sb.append(hex.toUpperCase());
+		}
+		return sb.toString();
+	}
+
+	public static byte[] parseHexStr2Byte(String hexStr) {
+		if (hexStr.length() < 1)
+			return null;
+		byte[] result = new byte[hexStr.length() / 2];
+		for (int i = 0; i < hexStr.length() / 2; i++) {
+			int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
+			int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2), 16);
+			result[i] = (byte) (high * 16 + low);
+		}
+		return result;
+	}
+}
---
+++ Code/Trader/src/util/OriginOrderFIXHelper.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.OriginOrder;
+public class OriginOrderFIXHelper {
+	public static String OriginOrder2Fix(OriginOrder oo){
+		MyFIX mf = new MyFIX("OriginOrder", "1", String.valueOf(oo.getBid()), 1);
+		mf.setTag(101, String.valueOf(oo.getFid()));
+		mf.setTag(102, String.valueOf(oo.getQuantity()));
+		mf.setTag(103, String.valueOf(oo.getPrice()));
+		mf.setTag(104, String.valueOf(oo.getStatus()));
+		mf.setTag(105, String.valueOf(oo.getBid()));
+		return mf.getFIX();
+	}
+	
+	public static List<OriginOrder> parseOriginOrder(String fixData){
+		List<OriginOrder> oos = new ArrayList<OriginOrder>();
+		MyFIX mf = new MyFIX(fixData);
+		int init_tag = 101;
+		while (mf.getTag(init_tag) != null){
+			OriginOrder oo = new OriginOrder();
+			oo.setFid(Integer.parseInt(mf.getTag((init_tag+1))));
+			oo.setQuantity(Integer.parseInt(mf.getTag((init_tag+2))));
+			oo.setPrice(Integer.parseInt(mf.getTag((init_tag+3))));
+			oo.setStatus(Integer.parseInt(mf.getTag((init_tag+4))));
+			oo.setBid(Integer.parseInt(mf.getTag((init_tag+5))));
+			init_tag = init_tag + 10;
+		}	
+		return oos;	
+	}
+}
