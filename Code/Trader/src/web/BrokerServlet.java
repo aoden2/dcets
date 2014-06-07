072ae417ef659b1bab6c6f67fc51df0462b925af
ifsoul <sjtuzyx@gmail.com>
2014-06-07 23:03:46 +0800

    dynamic page+servlets

--- Code/Trader/src/web/BrokerServlet.java
+++ Code/Trader/src/web/BrokerServlet.java
@@ -1,6 +1,8 @@
 package web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,11 +24,35 @@ public class BrokerServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+    protected void processRequest(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter(); 
+
+        try {            
+            String txt="<option value='option1'>ABC</option>"+
+          "<option value='option2'>BC1</option>"+
+            "<option value='option3'>BC2</option>"+
+            "<option value='option4'>BC3</option>";
+            
+            
+            //String t = new String(txt.getBytes("utf-8"),"iso-8859-1");
+            out.println(txt);
+            out.flush();
+        } catch(Exception e){
+        	e.printStackTrace();
+        }
+        finally {
+            out.close();
+        }
+    }
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		processRequest(request, response);
 	}
 
 	/**
@@ -34,6 +60,7 @@ public class BrokerServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		processRequest(request, response);
 	}
 
 }
--- Code/Trader/src/web/OrderServlet.java
+++ Code/Trader/src/web/OrderServlet.java
@@ -1,6 +1,8 @@
 package web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,12 +23,71 @@ public class OrderServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    protected void processRequest(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter(); 
+
+        try {
+            
+        	int id = Integer.parseInt(request.getParameter("id"));
+            //根据类型用ajax方式传送对应类型的电脑列表
+            //List<Computer> list =dao.PCList(type);
+            //String s = new String(type.getBytes("iso-8859-1"),"utf-8");
+            String txt="<thead>"+
+          "<tr>"+
+            "<th>Period</th>"+
+            "<th>Broker</th>"+
+            "<th>Qty</th>"+
+            "<th>Bid</th>"+
+            "<th>Ask</th>"+
+            "<th>Qty</th>"+
+            "<th>Broker</th>"+
+            "<th>Last</th>"+
+          "</tr>"+
+        "</thead>"+"<tbody>";
+            for(int i = 0;i < 5;i++){
+            	txt=txt+"<tr class=\"odd gradeX\">"+
+          "<td class=\"center\"> SEP14</td>"+
+          "<td class=\"center\"> ABC</td>"+
+          "<td class=\"center\"> 5</td>"+
+          "<td class=\"center\"> 1240</td>"+
+          "<td class=\"center\"> 1246</td>"+
+          "<td class=\"center\"> 10</td>"+
+          "<td class=\"center\"> ABC</td>"+
+          "<td class=\"center\"> 1241</td>"+
+          "</tr>"+
+          "<tr class=\"even gradeC\">"+
+            "<td class=\"center\"> SEP14</td>"+
+          "<td class=\"center\"> ABC</td>"+
+          "<td class=\"center\"> 5</td>"+
+          "<td class=\"center\"> 1240</td>"+
+          "<td class=\"center\"> 1246</td>"+
+          "<td class=\"center\"> 10</td>"+
+          "<td class=\"center\"> ABC</td>"+
+          "<td class=\"center\"> 1241</td>"+
+          "</tr>";
+            }
+            txt+="</tbody>";
+            if(id==-1){txt="";}
+            //String t = new String(txt.getBytes("utf-8"),"iso-8859-1");
+            out.println(txt);
+            out.flush();
+        } catch(Exception e){
+        	e.printStackTrace();
+        }
+        finally {
+            out.close();
+        }
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		processRequest(request, response);
 	}
 
 	/**
@@ -34,6 +95,7 @@ public class OrderServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		processRequest(request, response);
 	}
 
 }
