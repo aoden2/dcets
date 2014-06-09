a722c8ed70143d95c11d7d828fb7eadb266f2502
LKI <dg666@163.com>
2014-06-09 21:47:32 +0800

    New Trader FinalOrder

--- Code/Trader/src/entity/FinalOrder.java
+++ Code/Trader/src/entity/FinalOrder.java
@@ -2,51 +2,79 @@ package entity;
 
 public class FinalOrder {
 	private int id;
-	private int oid;
+	private int osid; // origin sell order id
+	private int obid; // origin buy order id
 	private int quantity;
 	private int price;
 	private int status;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	public int getOid() {
-		return oid;
+
+	public int getOsid() {
+		return osid;
 	}
-	public void setOid(int oid) {
-		this.oid = oid;
+
+	public void setOsid(int osid) {
+		this.osid = osid;
 	}
+
+	public int getObid() {
+		return obid;
+	}
+
+	public void setObid(int obid) {
+		this.obid = obid;
+	}
+
 	public int getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	public int getStatus() {
 		return status;
 	}
+
 	public void setStatus(int status) {
 		this.status = status;
 	}
-	public FinalOrder(int id, int oid, int quantity, int price, int status) {
+
+	public FinalOrder() {
+		super();
+	}
+
+	public FinalOrder(int id, int osid, int obid, int quantity, int price,
+			int status) {
 		super();
 		this.id = id;
-		this.oid = oid;
+		this.osid = osid;
+		this.obid = obid;
 		this.quantity = quantity;
 		this.price = price;
 		this.status = status;
 	}
-	public FinalOrder() {
-		super();
-		// TODO Auto-generated constructor stub
+
+	@Override
+	public String toString() {
+		return "|" + id + "," + osid + "," + obid + "," + quantity + ","
+				+ price + "," + status;
 	}
 
 }
