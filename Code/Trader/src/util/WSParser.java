73810cc2d773f9a14690e61042bb44c533338b94
LKI <dg666@163.com>
2014-06-09 21:57:36 +0800

    Changed WSParser

--- Code/Trader/src/util/WSParser.java
+++ Code/Trader/src/util/WSParser.java
@@ -1,7 +1,5 @@
 package util;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,21 +24,12 @@ public class WSParser {
 		return fo;
 	}
 
-	public static OriginOrder sToOO(String s) {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd ");
+	public static OriginOrder sToOO(String s, int bid) {
 		String ids[] = s.split(",");
 		OriginOrder oo = null;
-		try {
-			oo = new OriginOrder(Integer.parseInt(ids[0]),
-					Integer.parseInt(ids[1]), Integer.parseInt(ids[2]),
-					Integer.parseInt(ids[3]), Integer.parseInt(ids[4]),
-					Integer.parseInt(ids[5]), Integer.parseInt(ids[6]),
-					sdf.parse(ids[7]), Integer.parseInt(ids[8]));
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+		oo = new OriginOrder(Integer.parseInt(ids[0]),
+					Integer.parseInt(ids[1]), bid, Integer.parseInt(ids[3]),
+					Integer.parseInt(ids[6]), Integer.parseInt(ids[8]));
 		return oo;
 	}
 
@@ -60,11 +49,11 @@ public class WSParser {
 		return l;
 	}
 
-	public static List<OriginOrder> parseAllOriginOrder(String s) {
+	public static List<OriginOrder> parseAllOriginOrder(String s, int bid) {
 		List<OriginOrder> l = new ArrayList<>();
 		String str[] = s.split("\\|");
 		for (int i = 1; i < str.length; i++)
-			l.add(sToOO(str[i]));
+			l.add(sToOO(str[i], bid));
 		return l;
 	}
 
