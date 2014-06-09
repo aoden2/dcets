fc62d7b93d94bbee63093bc024415527caa9366f
Water-Drop <water_drop@foxmail.com>
2014-06-09 15:27:29 +0800

    add fidHelper

---
+++ Code/Trader/src/util/FidHelper.java
@@ -0,0 +1,10 @@
+package util;
+
+public class FidHelper {
+	public static int Broker2Trader(int bid, int bfid){
+		return 0;
+	}
+	public static int Trader2Broker(int bid, int tfid){
+		return 0;
+	}
+}
