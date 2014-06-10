f988122a183d98b3ca0e34c6e4df2068532986f3
Water-Drop <water_drop@foxmail.com>
2014-06-10 23:16:19 +0800

    add cancel order servlet

--- Code/Trader/src/web/CancelOrderServlet.java
+++ Code/Trader/src/web/CancelOrderServlet.java
@@ -1,11 +1,23 @@
 package web;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import util.MyClient;
+import util.OriginOrderFIXHelper;
+import dao.BrokerDao;
+import dao.BrokerDaoImpl;
+import dao.FutureDao;
+import dao.FutureDaoImpl;
+import dao.OrderDao;
+import dao.OrderDaoImpl;
+import entity.BrokerInfo;
+import entity.OriginOrder;
 
 /**
  * Servlet implementation class CancelOrderServlet
@@ -26,7 +38,20 @@ public class CancelOrderServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		int fid = Integer.parseInt(request.getParameter("fid"));
+		int ooid = Integer.parseInt(request.getParameter("id"));
+		System.out.println("Im in, canceling order");
+		int bid = 2;
+		BrokerDao bd = new BrokerDaoImpl();
+		BrokerInfo broker = bd.getBrokerbyId(bid);
+		MyClient c = null;
+			try {
+				String data = OriginOrderFIXHelper.revokeOrderFIX(fid, ooid, broker.getId());
+				c = new MyClient(broker.getIp(), broker.getPort(), broker.getPass());
+				String rtn = c.send(data);
+			} catch(Exception e) {
+				System.out.println("Error : " + e);
+			}
 	}
 
 	/**
